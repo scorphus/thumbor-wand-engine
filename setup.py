ed4512f131cc1fa07a88a214772d0b37988b9ba3
Jakob <jakob.zanker@googlemail.com>
2020-11-16 16:42:22 +0100

    setup: Require latest thumbor version
    Co-authored-by: Pablo Santiago Blum de Aguiar <scorphus@gmail.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ ImageMagick imaging engine for Thumbor.
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "thumbor<7.0.0",
+        "thumbor>=7.0.0a1",
         "Wand==0.6.*,>=0.6.3",
     ],
     extras_require={
