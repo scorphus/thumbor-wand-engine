a89fb081a8c9b0ba27f1197848014cdb577d2a16
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-03-17 20:52:33 +0100

    setup: Use README's content for `long_description`

--- setup.py
+++ setup.py
@@ -8,8 +8,15 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2020-2021, Pablo S. Blum de Aguiar <scorphus@gmail.com>
 
+from os import path
 from setuptools import find_packages
 from setuptools import setup
+
+
+def read_readme_contents():
+    file_dir = path.abspath(path.dirname(__file__))
+    with open(path.join(file_dir, "README.md"), encoding="utf-8") as f:
+        return f.read()
 
 
 tests_require = [
@@ -26,9 +33,8 @@ setup(
     name="thumbor_wand_engine",
     version="0.0.1",
     description="ImageMagick imaging engine for Thumbor.",
-    long_description="""
-ImageMagick imaging engine for Thumbor.
-""",
+    long_description=read_readme_contents(),
+    long_description_content_type="text/markdown",
     keywords="thumbor imaging imagemagick magickwand magick wand",
     author="Pablo S. Blum de Aguiar",
     author_email="scorphus@gmail.com",
