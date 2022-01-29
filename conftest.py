07efeb5b0cc9055abe4b46eaf9873e0f05a030d5
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-01-29 20:44:19 +0100

    conftest: Set specific SSIM precision for certain tests

--- conftest.py
+++ conftest.py
@@ -8,6 +8,8 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2020-2022, Pablo S. Blum de Aguiar <scorphus@gmail.com>
 
+from decimal import Decimal
+from decimal import getcontext
 from derpconf.config import Config
 from thumbor.testing import FilterTestCase
 from wand.image import Image
@@ -17,6 +19,12 @@ import pytest
 
 
 Config.allow_environment_variables()
+
+
+STRICT_TESTS_SSIM_PRECISION = {
+    "test_blur_filter_with_zero_radius": 2,
+    "test_rotate_filter_with_invalid_value": 2,
+}
 
 
 async def get_filtered(
@@ -35,16 +43,25 @@ def get_fixture(self, name, mode="RGB"):
     return image
 
 
-@staticmethod
-def get_ssim(actual, expected):
-    metric = "mean_absolute" if MAGICK_VERSION_INFO[0] > 6 else "mean_squared"
-    _, distortion = actual.compare(expected, metric)
-    return round(1 - distortion, 3)
+@pytest.fixture
+def get_ssim():
+    def get_ssim(precision=None):
+        def get_ssim(actual, expected):
+            metric = "mean_absolute" if MAGICK_VERSION_INFO[0] > 6 else "mean_squared"
+            _, distortion = actual.compare(expected, metric)
+            if precision is not None:
+                getcontext().prec = precision
+            return Decimal(1) - Decimal(distortion)
+
+        return staticmethod(get_ssim)
+
+    return get_ssim
 
 
 @pytest.fixture(autouse=True)
-def patch_get_filtered(mocker):
-    """Mokey-patch FilterTestCase's methods to use Wand instead of Pillow"""
+def auto_patch_filter_test_case_methods(mocker, request, get_ssim):
+    """Monkey patch FilterTestCase's methods to use Wand instead of Pillow"""
     mocker.patch.object(FilterTestCase, "get_filtered", get_filtered)
     mocker.patch.object(FilterTestCase, "get_fixture", get_fixture)
-    mocker.patch.object(FilterTestCase, "get_ssim", get_ssim)
+    precision = STRICT_TESTS_SSIM_PRECISION.get(request.node.name)
+    mocker.patch.object(FilterTestCase, "get_ssim", get_ssim(precision))
